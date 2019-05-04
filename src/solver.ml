2387e09ba320ca26bcc7d4753afbd095b9a5dff3
John Altidor <jgaltidor@gmail.com>
2019-05-04 01:05:35 -0400

    minor refactoring

--- src/solver.ml
+++ src/solver.ml
@@ -1,4 +1,6 @@
 open Script ;;
 open Core.Std ;;
 
-printf "input_board: %s\n" (Sudoku_board.string_of_board Main_inputs_test1.input_board) ;;
+let input_board = Main_inputs_test1.input_board ;;
+
+printf "input_board: %s\n" (Sudoku_board.string_of_board input_board) ;;
