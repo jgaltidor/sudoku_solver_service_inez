ebe427c75e466b6ebe21717b19d4e2d9f402a543
John Altidor <jgaltidor@gmail.com>
2019-05-07 19:02:16 -0400

    Updated solver.sh so that it can be executed from any directory rather than only sudoku_solver_inez/src

--- sudoku_solver_inez/src/solver.sh
+++ sudoku_solver_inez/src/solver.sh
@@ -1,2 +1,13 @@
 #! /usr/bin/env bash
-inez.top -noprompt -I $INEZ/frontend -init solver.init < $*
+SUDOKU_SOLVER_SRC=$(dirname "$0")
+INEZ=$HOME/custominstalls/inez_pkg/inez
+SCIP_SUITE_DIR=$HOME/custominstalls/inez_pkg/scipoptsuite-3.1.1
+# For OS X
+export DYLD_FALLBACK_LIBRARY_PATH=$SCIP_SUITE_DIR/lib:$DYLD_FALLBACK_LIBRARY_PATH
+# For Linux
+export LD_LIBRARY_PATH=$SCIP_SUITE_DIR/lib:$LD_LIBRARY_PATH
+export TMPDIR=$HOME/tmp
+$INEZ/frontend/inez.top -noprompt \
+  -I $INEZ/frontend \
+  -I $SUDOKU_SOLVER_SRC \
+  -init $SUDOKU_SOLVER_SRC/solver.init < $*
