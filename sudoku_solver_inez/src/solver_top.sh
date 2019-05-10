fa17a33e8aeee174edfaaf4fbe6d39e9468ac908
John Altidor <jgaltidor@gmail.com>
2019-05-09 22:03:41 -0400

    Updated script solver_top.sh to no longer depend on the current working directory or .bashrc

--- sudoku_solver_inez/src/solver_top.sh
+++ sudoku_solver_inez/src/solver_top.sh
@@ -1,2 +1,10 @@
 #! /usr/bin/env bash
-inez.top -I $INEZ/frontend -init solver.init $*
+SUDOKU_SOLVER_SRC=$(dirname "$0")
+INEZ=$HOME/custominstalls/inez_pkg/inez
+SCIP_SUITE_DIR=$HOME/custominstalls/inez_pkg/scipoptsuite-3.1.1
+export LD_LIBRARY_PATH=$SCIP_SUITE_DIR/lib:$LD_LIBRARY_PATH
+eval `opam config env`
+${INEZ}/frontend/inez.top \
+  -I $INEZ/frontend \
+  -I $SUDOKU_SOLVER_SRC \
+  -init $SUDOKU_SOLVER_SRC/solver.init $*
