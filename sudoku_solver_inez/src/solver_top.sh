119d3cffa202152bd0ff6f1557d8f6af114593b9
John Altidor <jgaltidor@gmail.com>
2019-05-09 22:47:33 -0400

    minor refactoring

--- sudoku_solver_inez/src/solver_top.sh
+++ sudoku_solver_inez/src/solver_top.sh
@@ -4,7 +4,7 @@ INEZ=$HOME/custominstalls/inez_pkg/inez
 SCIP_SUITE_DIR=$HOME/custominstalls/inez_pkg/scipoptsuite-3.1.1
 export LD_LIBRARY_PATH=$SCIP_SUITE_DIR/lib:$LD_LIBRARY_PATH
 eval `opam config env`
-${INEZ}/frontend/inez.top \
+$INEZ/frontend/inez.top \
   -I $INEZ/frontend \
   -I $SUDOKU_SOLVER_SRC \
   -init $SUDOKU_SOLVER_SRC/solver.init $*
