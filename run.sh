6d60e3a7595f05c0d7cd1f13bfff5b862434a148
John Altidor <jgaltidor@gmail.com>
2020-06-05 00:54:59 -0500

    made file path value of config solver_script_file a relative path

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 BASEDIR=$(dirname "$0")
 echo "Launching Backend Sudoku Service"
-$BASEDIR/SudokuServer/run.sh &
+pushd $BASEDIR/SudokuServer
+./run.sh &
+popd
 pushd $BASEDIR/sudoku_ui_prj/sudoku-ui
 echo "Launching Frontend Sudoku UI Web Server"
 npm start
